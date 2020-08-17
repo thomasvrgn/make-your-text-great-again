2d4277340ee2428c6433c52a751621f309759f3b
NessMC <contact@nessmc.fr>
2020-08-17 18:14:22 +0200

    ✨ Button component added.

---
+++ components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <button class="py-2 px-6 text-white font-semibold rounded text-sm focus:outline-none" :class="`bg-${color}-500 hover:bg-${color}-600 focus:bg-${color}-700`">
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    color: String
+  }  
+})
+</script>
---
+++ layouts/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <Button color="purple">
+    Upload photo
+  </Button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Button from '~/components/Button.vue'
+
+export default Vue.extend({
+  components: {
+    Button
+  }  
+})
+</script>
