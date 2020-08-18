76d8bc88d10490c9a38642f2005e0aff6c6d480d
NessMC <contact@nessmc.fr>
2020-08-18 15:17:19 +0200

    🔍 Meta tags updated.

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'Make your text great again with this website! Generate Trump memes with custom image and custom text.' },
+      { hid: 'image', name: 'og:image', content: '/your_text.png'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
